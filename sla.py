1efee5bc0e3e495cb0777d11b34ea0534f3f2d55
Tiago Pereira <clashfrog71@gmail.com>
2026-04-26 23:22:02 -0300

    organizando estrutura do codigo

--- sla.py
+++ sla.py
@@ -1,12 +1,22 @@
-print('opções')
-print('1 registrar um restaurante')
-print('2 listar restaurantes')
-opcao_escolhida = input()
-print(f'a opcao escolhida é {opcao_escolhida}')
-if opcao_escolhida == 1:
-   print('registrando restaurante')
-elif opcao_escolhida == 2:
-   print('listando restaurantes')
-else:
-   print('opcao invalida!')  
-   
+import os
+
+def tudo():
+   print('opções')
+   print('1 registrar um restaurante')
+   print('2 listar restaurantes')
+   opcao_escolhida = input()
+   def finalizar_sistema():
+      os.system('cls')
+      print('sistema finalizado')
+   print(f'a opcao escolhida é {opcao_escolhida}')
+   if opcao_escolhida == 1:
+      print('registrando restaurante')
+   elif opcao_escolhida == 2:
+      print('listando restaurantes')
+   else:
+      print('opcao invalida!')  
+def main():
+   tudo()
+if __name__ == '__main__': 
+   main() 
+  
