7ff3786296f46321ab460f95524238ce622845b2
Tiago Pereira <clashfrog71@gmail.com>
2026-05-03 00:29:41 -0300

    usando o match e resolvendo erros relacionados a o os.system

--- sla.py
+++ sla.py
@@ -1,20 +1,20 @@
-import os
-
+import subprocess
+import time
 def tudo():
    print('opções')
    print('1 registrar um restaurante')
    print('2 listar restaurantes')
-   opcao_escolhida = input()
-   print(f'a opcao escolhida é {opcao_escolhida}')
-   if opcao_escolhida == 1:
-      print('registrando restaurante')
-   elif opcao_escolhida == 2:
-      print('listando restaurantes')
-   else:
-      print('opcao invalida!')
-   def finalizar_sistema():
-      os.system('cls')
-      print('sistema finalizado')  
+   opcao_escolhida = int(input())
+   match opcao_escolhida:
+      case 1:
+         print('1 registrar um restaurante')
+      case 2:
+         print('2 listar restaurantes')
+      case _:
+         print('opção invalida! encerrando aplicação')
+         time.sleep(2)
+         subprocess.check_call("cls", shell=True)
+         print('aplicação encerrada')             
 def main():
    tudo()
 if __name__ == '__main__': 
