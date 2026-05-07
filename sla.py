b5a54df7e3f4d85adfa835ebeb9337b7cd07751f
Tiago Pereira <clashfrog71@gmail.com>
2026-05-06 23:46:11 -0300

    Adicionando o try catch e tambem um loop para caso o usuario escolha alguma opção não existente

--- sla.py
+++ sla.py
@@ -1,20 +1,27 @@
 import subprocess
 import time
 def tudo():
-   print('opções')
-   print('1 registrar um restaurante')
-   print('2 listar restaurantes')
-   opcao_escolhida = int(input())
-   match opcao_escolhida:
-      case 1:
-         print('1 registrar um restaurante')
-      case 2:
-         print('2 listar restaurantes')
-      case _:
-         print('opção invalida! encerrando aplicação')
-         time.sleep(2)
-         subprocess.check_call("cls", shell=True)
-         print('aplicação encerrada')             
+   try:
+      print('opções')
+      print('1 registrar um restaurante')
+      print('2 listar restaurantes')
+      opcao_escolhida = int(input())
+      match opcao_escolhida:
+         case 1:
+            print('1 registrar um restaurante')
+         case 2:
+            print('2 listar restaurantes')
+         case _:
+            print('opção invalida! encerrando aplicação')
+            time.sleep(2)
+            subprocess.check_call("cls", shell=True)
+            print('aplicação encerrada')             
+   except:
+      opcao_invalida()         
+def opcao_invalida():
+   print('Opção Ivalida')
+   input('aperte qualquer tecla para voltar')
+   main()
 def main():
    tudo()
 if __name__ == '__main__': 
