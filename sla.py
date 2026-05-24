ec322de2061dbd0889928963fbb5daa0a6492417
Tiago Pereira <clashfrog71@gmail.com>
2026-05-24 00:02:33 -0300

    adicionando nova função de cadastrar restaurante e saldo ele em uma lista

--- sla.py
+++ sla.py
@@ -1,14 +1,16 @@
 import subprocess
 import time
+restaurantes = []
 def tudo():
    try:
       print('opções')
       print('1 registrar um restaurante')
       print('2 listar restaurantes')
+      print('3 Sair da aplicação')
       opcao_escolhida = int(input())
       match opcao_escolhida:
          case 1:
-            print('1 registrar um restaurante')
+            cadastrar_restaurante()
          case 2:
             print('2 listar restaurantes')
          case 3:
@@ -24,7 +26,16 @@ def tudo():
             print('aplicação encerrada')             
    except:
       
-      opcao_invalida()         
+      opcao_invalida()
+def cadastrar_restaurante():
+   subprocess.check_call("cls", shell=True)
+   print('cadastrar restaurante')
+   nomeRestaurante = input('digite o nome do restaurante que deseja cadastrar')
+   restaurantes.append(nomeRestaurante)
+   print(f'o {nomeRestaurante} foi cadastrado com sucesso!')
+   input('digite qualquer tecla para sair')
+   main()
+
 def opcao_invalida():
    print('Opção Ivalida')
    input('aperte qualquer tecla para voltar')
